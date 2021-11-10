1382a9810b1de313975cbe37e1a514e74e4edadd
P4lerm0 <90239535+P4lerm0@users.noreply.github.com>
2021-11-10 09:10:42 +0300

    Add files via upload

--- ConsoleApp9/ConsoleApp9/Program.cs
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -1,19 +1,28 @@
-﻿//f(x, y, z) = !x & z | !y | !x = false
-int x, y, z;
-3x=Convert.ToInt32(Console.ReadLine());
-y=Convert.ToInt32(Console.ReadLine());
-z=Convert.ToInt32(Console.ReadLine());
-bool c = Convert.ToBoolean(x);
-bool d = Convert.ToBoolean(y);
-bool e = Convert.ToBoolean(z);
+﻿bool x, y, z;
+NewMethod(out x, out y, out z);
+Console.WriteLine(!x & z | !y | !x);
 
-bool f = !c & e | !d | !c;
-if (f)
+bool x1 = true, y1 = true, z1 = true;
+Console.WriteLine(!x & z | !y | !x);
+
+bool x2 = false, y2 = false, z2 = true;
+Console.WriteLine(!x & z | !y | !x);
+
+bool x3 = true, y3 = false, z3 = false;
+Console.WriteLine(!x & z | !y | !z);
+
+bool x4 false, y4 = true, z4 = false;
+Console.WriteLine(!x & z | !y | !z);
+
+bool x5 = true, y5 = true, z5 = false;
+Console.WriteLine(!x & z | !y | !x);
+
+bool x6 = false, y6 = false, z6 = false;
+Console.WriteLine(!x & z | !y | !z);
+
+private static void NewMethod(out bool x, out bool y, out bool z)
 {
-    Console.WriteLine(true);
+    x = false;
+    y = false;
+    z = false;
 }
-else
-{
-    Console.WriteLine(false);
-}
-Console.ReadKey();
